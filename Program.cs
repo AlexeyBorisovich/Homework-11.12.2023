9d71174fd8d6197eaf27ec676753ac16bfb1ef7b
AlexeyBorisovich <a.senior@internet.ru>
2023-12-10 23:44:28 +0600

    Задение 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+
+        bool isDivisible = CheckDivisibility(userNumber);
+
+        if (isDivisible)
+        {
+            Console.WriteLine($"{userNumber} кратно 7 и 23.");
+        }
+        else
+        {
+            Console.WriteLine($"{userNumber} не кратно одновременно 7 и 23.");
+        }
+    }
+    static bool CheckDivisibility(int number)
+    {
+        return (number % 7 == 0) && (number % 23 == 0);
+    }
+}
